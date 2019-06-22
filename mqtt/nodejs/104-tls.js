2936128c68e9550e7d52ef16d70d11cead4bddb7
Oleg Prohazko <olegprohazko@gmail.com>
2019-06-22 18:35:13 +0300

    node example tls listen

--- mqtt/nodejs/104-tls.js
+++ mqtt/nodejs/104-tls.js
@@ -19,6 +19,10 @@ const key = `
 const mqtt = require('mqtt')
   .connect('mqtts://sandbox.rightech.io', { key, cert });
 
+mqtt.on('message', (topic, message) => {
+  console.log(`${new Date().toISOString()}: [${topic}] ${message.toString()}`);
+});  
+ 
 setInterval(() => {
   const rand = (Math.random() * 30).toFixed(2);
   mqtt.publish('base/state/temperature', rand)
