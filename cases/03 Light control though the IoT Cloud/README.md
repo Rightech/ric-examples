12c6fac6cea4484e6adc5c10a6470c0821e2854b
Kristina <k.goldinova@rightech.io>
2021-03-30 12:51:06 +0300

    add pub+sub json

--- cases/03 Light control though the IoT Cloud/README.md
+++ cases/03 Light control though the IoT Cloud/README.md
@@ -1,6 +1,6 @@
-# How to control light through the IoT cloud platform using esp8266 (on the example of the LED)
+# How to control light through the IoT cloud platform using ESP8266 (on the example of the RGB LED)
 
-- 103_2-json.ino - arduino sketch with light control(LED);
-- _mqtt__Model_01.json - model of device for Rightech IoT Cloud.
+- *103_2-json.ino*, *103_3-json.ino* - arduino sketches with light control (LED);
+- *_mqtt__Model_01.json* - model of device for Rightech IoT Cloud.
 
-Watch more on [youtube](https://www.youtube.com/watch?v=3lDJWkMEh88&list=PLb9vz8ebECgXBgilNF5UF7j01h2xWS-3I&index=3)
+Watch more on [YouTube](https://www.youtube.com/watch?v=3lDJWkMEh88&list=PLb9vz8ebECgXBgilNF5UF7j01h2xWS-3I&index=3)
--- mqtt/arduino/103-json.ino
+++ mqtt/arduino/103-json.ino
@@ -1,17 +1,17 @@
 /**
- For this particular example you could try this model with JSON params pre-configured: 
+  For this particular example you could try this model with JSON params pre-configured:
     https://github.com/Rightech/ric-examples/blob/master/.ric-models/mqtt-example-003-json.ric-model.json
- Note that: 
-    @node `json`        - only "must have" for this example 
+  Note that:
+    @node `json`        - only "must have" for this example
                         ( although you could rename all the nodes/fields as you like )
     @node `server-info` - for connectivity debug
     @node `mqtt-last`   - for mqtt publish debug
 */
 
 #include "Arduino.h"
-#include "EspMQTTClient.h" /* https://github.com/plapointe6/EspMQTTClient */
-                           /* https://github.com/knolleary/pubsubclient */
-#include "ArduinoJson.h"   /* https://github.com/bblanchon/ArduinoJson */
+#include "EspMQTTClient.h"  /* https://github.com/plapointe6/EspMQTTClient */
+                            /* https://github.com/knolleary/pubsubclient */
+#include "ArduinoJson.h"    /* https://github.com/bblanchon/ArduinoJson */
 
 #define PUB_DELAY (5 * 1000) /* 5 seconds */
 
@@ -24,11 +24,25 @@ EspMQTTClient client(
 );
 
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(9600);
 }
 
 void onConnectionEstablished() {
   Serial.println("connected");
+  client.subscribe("led_on", [] (const String & payload) {
+    ledOn(payload);
+  });
+}
+
+void ledOn(const String & payload) {
+  StaticJsonDocument<1024> led;
+  deserializeJson(led, payload);
+  String redValue   = led["red"];
+  String greenValue = led["green"];
+  String blueValue  = led["blue"];
+  Serial.println("RED:"   + redValue);
+  Serial.println("GREEN:" + greenValue);
+  Serial.println("BLUE:"  + blueValue);
 }
 
 long last = 0;
@@ -38,7 +52,7 @@ void publishTemperature() {
     StaticJsonDocument<1024> doc;
     doc["temperature"] = random(20, 30);
     doc["humidity"] = random(40, 90);
-    
+
     String payload;
     serializeJson(doc, payload);
 
